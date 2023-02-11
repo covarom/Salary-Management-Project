29966b7c77e2096c4de6d54cc91a0cf49091431e
Truong Giang <122654316+giangnt55@users.noreply.github.com>
2023-02-12 02:03:49 +0700

    contract features - add get all, get by id

---
+++ SalaryManagement.Application/Services/ContractServices/ContractService.cs
@@ -0,0 +1,26 @@
+﻿
+using SalaryManagement.Application.Common.Interfaces.Persistence;
+using SalaryManagement.Domain.Entities;
+
+namespace SalaryManagement.Application.Services.ContractServices
+{
+    public class ContractService : IContractServices
+    {
+        private readonly IContractRepository _contractRepository;
+
+        public ContractService(IContractRepository contractRepository)
+        {
+            _contractRepository = contractRepository;
+        }
+
+        public async Task<IEnumerable<Contract>> GetAllContracts()
+        {
+            return await _contractRepository.GetAllContractsAsync();
+        }
+
+        public async Task<Contract> GetById(string contractId)
+        {
+            return await _contractRepository.GetContractByIdAsync(contractId);
+        }
+    }
+}
---
+++ SalaryManagement.Contracts/Contracts/ContractRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace SalaryManagement.Contracts.Contracts
+{
+    public record ContractRequest(
+        string Id,
+        string File,
+        DateTime StartDate,
+        DateTime EndDate,
+        string Job,
+        string BacsicSalary,
+        string BHXH,
+        string Partner,
+        string PartnerPrice,
+        string EmployeeId,
+        string ContractTypeId,
+        string SalaryTypeId,
+        string ContractStatusId
+        );
+}
