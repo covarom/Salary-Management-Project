bf5e578d343c63469631e09a5bb11118553510d8
Truong Giang <122654316+giangnt55@users.noreply.github.com>
2023-02-14 18:16:14 +0700

    update contract api

--- SalaryManagement.Api/Controllers/ContractsController.cs
+++ SalaryManagement.Api/Controllers/ContractsController.cs
@@ -2,14 +2,16 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SalaryManagement.Application.Services.ContractServices;
+using SalaryManagement.Contracts;
 using SalaryManagement.Contracts.Contracts;
+using SalaryManagement.Domain.Entities;
 using System.Net;
 
 namespace SalaryManagement.Api.Controllers
 {
     [Route("api/v1")]
     [ApiController]
-  
+    [Authorize]
     public class ContractsController : ControllerBase
     {
         private readonly IContractServices _contractService;
@@ -26,27 +28,76 @@ namespace SalaryManagement.Api.Controllers
         {
             var contracts = await _contractService.GetAllContracts();
 
-            return Ok(contracts);
+            return Ok(new Response<object>
+            {
+                Data = contracts,
+                StatusCode = (int)HttpStatusCode.OK,
+                Message = string.Empty
+            });
         }
 
         [HttpGet("contracts/{id}")]
         public async Task<IActionResult> Find(string id)
         {
             var contracts = await _contractService.GetById(id);
-            return Ok(contracts);
+            if (contracts == null)
+            {
+                return NotFound(new Response<object>
+                {
+                    Data = null,
+                    Message = "Not found",
+                    StatusCode =(int)HttpStatusCode.NotFound
+                });
+            }
+
+            return Ok(new Response<object>
+            {
+                Data = contracts,
+                Message = string.Empty,
+                StatusCode = (int)HttpStatusCode.OK
+            });
         }
 
         [HttpPost("contracts")]
-        public async Task<IActionResult> AddContract(ContractRequest request)
+        public async Task<IActionResult> AddContract([FromBody]SaveContractRequest request)
         {
             await Task.CompletedTask;
-            /*
-             Do the bussiness logic
-             */
+            var contract = new Contract
+            {
+                ContractId = Guid.NewGuid().ToString(),
+                File = request.File,
+                StartDate = request.StartDate,
+                EndDate = request.EndDate,
+                Job = request.Job,
+                BasicSalary = request.BasicSalary,
+                Bhxh = request.BHXH,
+                Partner = request.Partner,
+                PartnerPrice = request.PartnerPrice,
+                EmployeeId = request.EmployeeId,
+                ContractTypeId = request.ContractTypeId,
+                SalaryTypeId = request.SalaryTypeId,
+                ContractStatusId = request.ContractStatusId
+            };
 
-            //var contracts = await _contractService.GetById(id);
-            return Ok(request);
+            var addedContract = await _contractService.AddContractAsync(contract);
+
+            return CreatedAtAction(nameof(Find), new { id = contract.ContractId }, addedContract);
+
         }
 
-    }
+
+        [HttpDelete("contract/{id}")]
+        public async Task<IActionResult> DeleteContract(string id)
+        {
+            await _contractService.DeleteContractAsync(id);
+  /*          if (existingContract == null)
+            {
+                return NotFound();
+            }
+*/
+           // await _contractService.DeleteContractAsync(existingContract);
+            return NoContent();
+        }
+
+    }   
 }
