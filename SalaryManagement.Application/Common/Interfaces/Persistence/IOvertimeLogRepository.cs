230be9641d9aa8919b99c216bee3a5eb160ed9e7
Lê Thiên Ân <80709126+ThienAnn-SE@users.noreply.github.com>
2023-02-14 15:45:02 +0700

    Add new overtimeLog repo, services, controller

---
+++ SalaryManagement.Application/Common/Interfaces/Persistence/IOvertimeLogRepository.cs
@@ -0,0 +1,18 @@
+﻿using SalaryManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryManagement.Application.Common.Interfaces.Persistence
+{
+    public interface IOvertimeLogRepository
+    {
+        public Task<IEnumerable<OvertimeLog>> GetAllOvertimeLogs();
+        public Task<OvertimeLog> GetOvertimeLogById(string id);
+        public Task<OvertimeLog> AddNewOvertimeLog(OvertimeLog overtimeLog);
+        public Task<OvertimeLog> UpdateOvertimeLog(OvertimeLog overtimeLog);
+        public Task DeleteOvertimeLog(string id);
+    }
+}
