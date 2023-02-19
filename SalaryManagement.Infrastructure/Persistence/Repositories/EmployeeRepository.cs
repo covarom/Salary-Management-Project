0224f795b3407f20ebdae53c54258cdb1bae66b0
Lê Thiên Ân <80709126+ThienAnn-SE@users.noreply.github.com>
2023-02-19 20:06:49 +0700

    fix conflict (#34)

--- SalaryManagement.Infrastructure/Persistence/Repositories/EmployeeRepository.cs
+++ SalaryManagement.Infrastructure/Persistence/Repositories/EmployeeRepository.cs
@@ -66,9 +66,5 @@ namespace SalaryManagement.Infrastructure.Persistence.Repositories
             employee = _context.Employees.SingleOrDefault(e => e.Name.Contains(EmployeeName.Trim()));
             return employee.EmployeeId;
         }
-
-
-            return employee.EmployeeId;
-        }
     }
 }
